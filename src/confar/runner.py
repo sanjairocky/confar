0e8c33f6c7120290a8e8b2d93c09351b12355224
Sanjai Kumar Arumugam <Sanjai Kumar Arumugam sanjai.kumar.arumugam@walmart.com>
2023-10-08 21:01:43 +0530

    bugfix

--- src/confar/runner.py
+++ src/confar/runner.py
@@ -12,18 +12,20 @@ class Runner:
         result = {'name': step['name']}
         try:
             if 'skip' in step and step['skip']:
-                result['result'] = ['step skipped']
+                result['result'] = 'step skipped'
             elif 'env' in step:
+                result['cmd'] = []
                 for k, v in step['env'].items():
                     self.ctx[k] = str(str(v).replace(
                         '{$.', '{')).format(**self.ctx.__dict__)
-                result['result'] = ['added env']
+                    result['cmd'].append(f"export {k} = {self.ctx[k]}")
+                result['result'] = 'added env'
             elif 'shell' in step:
                 text = str(str(step['shell']).replace(
                     '{$.', '{')).format(**self.ctx.__dict__)
                 result['cmd'] = f'Executing shell cmd : {text}'
-                result['result'] = (subprocess.check_output(
-                    text, encoding='UTF-8')).splitlines()
+                result['result'] = (subprocess.getoutput(
+                    text.split(), encoding='UTF-8')).splitlines()
             elif 'call' in step:
                 result['cmd'] = f'Invoking confar sub-flow : {step["call"]}'
                 result['flows'] = self.execute_steps(flow=step['call'])
@@ -35,7 +37,7 @@ class Runner:
                     result['res'] = [self.execute_step(step=the)
                                      for the in step['else']]
                 else:
-                    result['result'] = ['step skipped']
+                    result['result'] = 'step skipped'
         except Exception as e:
             result['error'] = f'error : {str(e)}'
         return result
